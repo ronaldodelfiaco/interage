1fb75a6ae2b691f6ec7f1f0e8e25765dac22817b
GabrielMeloBatista <gabrielmelobatisa@hotmail.com>
2022-05-04 09:46:08 -0300

    Correções com o tema

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,35 +1,31 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 import { LocalizationProvider } from '@mui/lab';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
-
-import Link from 'next/link';
-import TitleContextProvider from '../contexts/TitleContext';
-import { AuthProvider } from '../contexts/AuthContext';
-import SettingsProvider from '../contexts/SettingsContext';
 import {
   CssBaseline,
   StyledEngineProvider,
-  ThemeProvider,
+  ThemeProvider
 } from '@mui/material';
-import { ukoTheme } from '../theme';
-import useSettings from '../hooks/useSettings';
-import { useEffect } from 'react';
-import router from 'next/router';
-import RTL from '../components/RTL';
-import DashboardLayout from '../components/Layouts/DashboardLayout';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Toaster } from 'react-hot-toast';
 import AuthGuard from '../components/authentication/AuthGuard';
+import DashboardLayout from '../components/Layouts/DashboardLayout';
+import RTL from '../components/RTL';
+import { AuthProvider } from '../contexts/AuthContext';
+import SettingsProvider from '../contexts/SettingsContext';
+import TitleContextProvider from '../contexts/TitleContext';
+import useSettings from '../hooks/useSettings';
+import '../styles/globals.css';
+import styles from '../styles/Home.module.css';
+import { ukoTheme } from '../theme';
+
 
 function InterageApp({ Component, pageProps }: AppProps) {
   const { settings } = useSettings();
 
   // Configuração do tema
   const appTheme = ukoTheme({
-    theme: 'dark',
+    theme: settings.theme,
     direction: settings.direction,
     responsiveFontSizes: settings.responsiveFontSizes,
   });
