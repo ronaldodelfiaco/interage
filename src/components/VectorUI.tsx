64ae2aba0a5891afe0d3573132b03d57e6e1faaa
GabrielMeloBatista <gabrielmelobatisa@hotmail.com>
2022-07-25 09:07:59 -0300

    Renomeado o visualizador do vetor

--- src/components/VectorUI.tsx
+++ src/components/VectorUI.tsx
@@ -1,25 +1,21 @@
-import { Box, Card, Grid, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 import axios from 'axios';
-import {
-  Dispatch,
-  FC,
-  MouseEvent,
-  SetStateAction,
-  useEffect,
-  useState,
-} from 'react';
-import AddIconButton from './AddIconButton';
-import FlexBox from './FlexBox';
-import GenericModal from './itens/GenericModal';
-import ListGeneric from './ListGeneric';
+import { Dispatch, FC, MouseEvent, SetStateAction, useState } from 'react';
 import MoreOptions from './MoreOptions';
-import { Tiny } from './Typography';
+
+interface ListCardProps {
+  item: any;
+  handleMore: (event: MouseEvent<HTMLButtonElement>) => void;
+  setID: (index: number) => void;
+  index: number;
+}
 
 interface GenericVectorUProps {
   Array: Array<any>;
   setItem: Dispatch<SetStateAction<any>>;
   setEdit: Dispatch<SetStateAction<any>>;
   setOpenModal: Dispatch<SetStateAction<any>>;
+  ListCard: FC<ListCardProps>;
   heroku?: string;
 }
 
@@ -28,6 +24,7 @@ const GenericVectorUI: FC<GenericVectorUProps> = ({
   setItem,
   setEdit,
   setOpenModal,
+  ListCard,
   heroku,
 }) => {
   // MoreOptions
@@ -61,41 +58,42 @@ const GenericVectorUI: FC<GenericVectorUProps> = ({
       .catch((error) => {
         console.error(error);
       });
+
+    // Chamado para apagar visualmente.
+    Erase();
     handleGenericMoreClose();
   };
 
   return (
-    <Card sx={{ padding: 3, pb: 4 }}>
-      <Grid container spacing={3} pt={3}>
-        {Array.map((value, index) => (
-          <Grid item xs={12} sm={6} key={value}>
-            {/* Criar Generic ListCard */}
-            <ListGeneric
-              setID={setIdEdit}
-              index={index}
-              item={value}
-              handleMore={handleGenericMoreOpen}
-            />
-          </Grid>
-        ))}
-        <MoreOptions
-          id={idEdit}
-          anchorEl={genericEl}
-          handleMoreClose={handleGenericMoreClose}
-          editar={Edit}
-          apagar={!heroku ? Erase : EraseHeroku}
-        />
-        <Grid item xs={12} sm={6}>
-          <FlexBox alignItems="center">
+    <>
+      {Array.map((value, index) => (
+        <Grid item xs={12} sm={6} key={value}>
+          {/* Criar Generic ListCard */}
+          <ListCard
+            setID={setIdEdit}
+            index={index}
+            item={value}
+            handleMore={handleGenericMoreOpen}
+          />
+        </Grid>
+      ))}
+      <MoreOptions
+        id={idEdit}
+        anchorEl={genericEl}
+        handleMoreClose={handleGenericMoreClose}
+        editar={Edit}
+        apagar={!heroku ? Erase : EraseHeroku}
+      />
+      {/* <Grid item xs={12} sm={6}>
+         <FlexBox alignItems="center">
             <AddIconButton onClick={() => setOpenModal(true)} />
             <Box ml="1rem">
               <Typography variant="h6">Adicionar</Typography>
               <Tiny color="secondary.400">novo valor</Tiny>
             </Box>
-          </FlexBox>
-        </Grid>
-      </Grid>
-    </Card>
+          </FlexBox> 
+      </Grid>*/}
+    </>
   );
 };
 
